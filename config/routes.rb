3560775e2c499e4af4b374105b04841ca43be0f4
jjswski i <jjswski@gmail.com>
2019-02-17 15:07:05 +0100

    Add root home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'high_voltage/pages#show', id: 'home'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
