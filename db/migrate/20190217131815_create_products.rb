5fe59f38c0c2fe984c4cf4ee8bbba13dd881497e
jjswski i <jjswski@gmail.com>
2019-02-17 15:05:29 +0100

    Do create_products migration

--- db/migrate/20190217131815_create_products.rb
+++ db/migrate/20190217131815_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
     create_table :products do |t|
       t.string :name
       t.text :description
-      t.deleted_at :datatime
+      t.timestamp :deleted_at
 
       t.timestamps
     end
